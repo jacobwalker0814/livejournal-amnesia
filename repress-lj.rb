6cb381b6b283de2cf700512d145c4d69fb2e72d9
Jacob Walker <jwalker@tortugas-llc.com>
2014-10-30 16:05:44 -0500

    Handle mising/invalid credentials.

--- repress-lj.rb
+++ repress-lj.rb
@@ -3,6 +3,17 @@ require 'dotenv'
 
 Dotenv.load
 
-user = LiveJournal::User.new(ENV['LJ_USERNAME'],ENV['LJ_PASSWORD'])
-login = LiveJournal::Request::Login.new(user)
-login.run
+unless ENV.has_key?('LJ_USERNAME') && ENV.has_key?('LJ_PASSWORD');
+  puts "Missing environment variable LJ_USERNAME and/or LJ_PASSWORD"
+  exit
+end
+
+begin
+  puts "Logging in to LiveJournal..."
+  user = LiveJournal::User.new(ENV['LJ_USERNAME'],ENV['LJ_PASSWORD'])
+  login = LiveJournal::Request::Login.new(user)
+  login.run
+rescue Exception => e
+  puts e.message
+  exit
+end
